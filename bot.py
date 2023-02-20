913edede0ff400165a5c527acbbae0c9f895651d
Pirated <71497650+ItsYash27@users.noreply.github.com>
2023-02-20 19:28:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,12 +7,11 @@
 from os import environ
 # Moved Back to asyncio-dev branch of pyrogram
 from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup
-import pyshorteners
+import PyBypass as bypasser
 
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
 BOT_TOKEN = environ.get('BOT_TOKEN')
-API_KEY = environ.get('API_KEY')
 
 bot = Client('Shortlink bot',
              api_id=API_ID,
@@ -25,30 +24,19 @@ bot = Client('Shortlink bot',
 @bot.on_message(Filters.command('start') & Filters.private)
 async def start(bot, update):
     await update.reply(
-        f"**Hi {update.chat.first_name}!**\n\n"
-        "I'm shortlink bot. Just send me link and get adsless short link")
+        f"**Hello🌝 {update.chat.first_name}!**\n\n"
+        "I'm A Bot To Unshorten The Link. I support many sites\n This is Still In Beta Testing @Crazy_Yadh")
 
 
 @bot.on_message(Filters.regex(r'https?://[^\s]+') & Filters.private)
 async def link_handler(bot, update):
     link = update.matches[0].group(0)
-    if API_KEY:
       try:
-        s = pyshorteners.Shortener(api_key=API_KEY) 
-        shortened_url = s.cuttly.short(link)
-        button = [[InlineKeyboardButton("Link 🔗", url=shortened_url)]]
+        bypassed_link = bypasser.bypass() 
+        bypassed_link = bypasser.bypass(link)
+        button = [[InlineKeyboardButton("ʙʏᴘᴀssᴇᴅ ʟɪɴᴋ", url=bypassed_link)]]
         markup = InlineKeyboardMarkup(button)
-        await update.reply_text(text=f'Here is your shortlink \n`{shortened_url}`', reply_markup=markup, quote=True)
-        
-      except Exception as e:
-        await update.reply(f'Error: {e}', quote=True)
-    else:
-      try:
-        s = pyshorteners.Shortener() 
-        shortened_url = s.dagd.short(link)
-        button = [[InlineKeyboardButton("Link 🔗", url=shortened_url)]]
-        markup = InlineKeyboardMarkup(button)
-        await update.reply_text(text=f'Here is your shortlink \n`{shortened_url}`', reply_markup=markup, quote=True)
+        await update.reply_text(text=f'Here is your shortlink \n`{bypassed_link}`', reply_markup=markup, quote=True)
         
       except Exception as e:
         await update.reply(f'Error: {e}', quote=True)
